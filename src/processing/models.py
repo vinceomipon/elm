6904693acb9ed049c18ddcca0d16f4329db07dab
vinceomipon <vomipon@student.ubc.ca>
2026-05-18 11:20:54 -0700

    testing commit

--- src/processing/models.py
+++ src/processing/models.py
@@ -1,6 +1,7 @@
 # import libraries
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 class ELMImage:
 
@@ -11,23 +12,47 @@ class ELMImage:
         image: the image to reference to, must have valid path to file
 
     """
-    def __init__(self, image):
-        self._image = image
-        self._bgr_arr = cv2.imread(image)
-        self._rgb_arr = cv2.cvtColor(self._bgr_arr, cv2.COLOR_BGR2RGB)
-        self._hsv_arr = cv2.cvtColor(self._bgr_arr, cv2.COLOR_BGR2HSV)
-        self._illuminated_mask = None
-        self._panel_mask = None
+    def __init__(self, dir: str):
+        self.dir = dir
+        self.bgr_arr = cv2.imread(dir)
+        self.rgb_arr = cv2.cvtColor(self.bgr_arr, cv2.COLOR_BGR2RGB)
+        self.hsv_arr = cv2.cvtColor(self.bgr_arr, cv2.COLOR_BGR2HSV)
+        self.illuminated_mask = None
+        self.panel_mask = None
     
 
     # Define get methods
 
-    def get_image(self):
-        return self._image
+    def get_directory(self):
+        return self.dir
     
     def get_bgr_arr(self):
-        return self._bgr_arr
+        return self.bgr_arr
     
     def get_hsv_arr(self):
-        return self._hsv_arr
+        return self.hsv_arr
+    
+    def get_rgb_arr(self):
+        return self.rgb_arr
+    
+    # display scatter plots
+    def disp_rgb_scatter(self):
+        # create 3d plot and figure
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+
+
+        r, g, b = cv2.split(self.rgb_arr)
+        cv2.imshow('Blue Channel', b)
+        cv2.waitKey(0)
+    
+    
+    # Permanently resizes the image
+    # returns True if succesfully completed
+    def resize_img(self, width: int, height: int):
+        self._bgr_arr = cv2.resize(self.bgr_arr, (width, height))
+        self._rgb_arr = cv2.resize(self.rgb_arr, (width, height))
+        self._hsv_arr = cv2.resize(self.hsv_arr, (width, height))
+        return True
+
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -5,10 +5,10 @@ import numpy as np
 from src.processing.models import ELMImage
 
 # checks getter methods
-def test_get_image():
+def test_get_dir():
     dir = "data/raw/20241116_110324.jpg"
     img = ELMImage("data/raw/20241116_110324.jpg")
-    assert dir == img.get_image()
+    assert dir == img.get_directory()
 
 def test_get_bgr_arr():
     dir = "data/raw/20241116_110324.jpg"
@@ -40,5 +40,33 @@ def test_rgb_shape():
     rgb_arr = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     print(rgb_arr.shape)
     print(rgb_arr)
+
+def test_resize_img():
+    dir = "data/raw/20241116_110324.jpg"
+    img = ELMImage(dir)
+    expected_img = cv2.imread(dir)
+    
+
+    height = 400
+    width = 300
+
+    # this is true since 'cv2.imread(dir)' outputs a bgr_arr
+    expected_bgr_arr = expected_img
+    expected_hsv_arr = cv2.cvtColor(expected_img, cv2.COLOR_BGR2HSV)
+
+    # resize the image
+    expected_bgr_arr = cv2.resize(expected_bgr_arr, (width, height))
+    expected_hsv_arr = cv2.resize(expected_hsv_arr, (width, height))
+
+    # check if resize was performed sucessfully
+    assert img.resize_img(width, height)
+
+    # actual_rgb_arr = img._rgb_arr
+
+
+
+
+
+
     
     
